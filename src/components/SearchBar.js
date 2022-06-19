95adfb0c9b5899c8aaca1067425e04f9fcc85231
J.Reczko <reczko.jakub@gmail.com>
2022-06-19 14:44:03 +0200

    Add input change handler to the search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (e) => {
+        this.setState({term: e.target.value});
+    } 
+
     render() {
         return (
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Video Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>
