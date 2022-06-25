09ea2adf43267ee47724a199a7597c03743bb13d
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 11:32:53 +0200

    Style the description segment of the  video details component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,10 +4,13 @@ const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading...</div>
     }
-    
+
     return (
-        <div className="video-detail">
-            {video.snippet.title}
+        <div className="video-detail ">
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
     )
 }
