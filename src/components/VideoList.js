d09397660007f61f9b0fc4d040e7916a4bba4312
J.Reczko <reczko.jakub@gmail.com>
2022-06-25 10:48:45 +0200

    Rollback call handler function

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,9 +6,7 @@ const VideoList = ({
     onVideoSelect
 }) => {
     const videosList = videos.map(video => (
-        <VideoItem onClick={() => {
-            return onVideoSelect(video);
-        }} key={video.id.videoId} video={video} />
+        <VideoItem key={video.id.videoId} video={video} />
     ));
     return (
         <div className='video-list ui relaxed divided list'>
