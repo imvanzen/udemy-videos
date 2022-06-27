788ede98024d022e3bfa44dc5bc08ad7dbd948af
J.Reczko <reczko.jakub@gmail.com>
2022-06-27 09:18:40 +0200

    Secure API key by using .env file

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCR5C2Qyv47B-W842NkUSv1svW8xeYeJY8';
-
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: KEY
+        key: process.env.REACT_APP_YOUTUBE_API_KEY
     }
 });
